7528197cdef594e91d8ac6aa9d8fce2f30a62a5b
Capellan Miguel <capellanvazquezmiguelangel@johndeere.com>
2023-11-26 18:49:36 +0100

    Add slug to category

--- cms-sanity/schemas/documents/category.ts
+++ cms-sanity/schemas/documents/category.ts
@@ -18,6 +18,19 @@ export default defineType({
   },
   fields: [
     defineField({
+      name: 'slug',
+      title: 'Slug',
+      type: 'slug',
+      options: {
+        source: (doc: any) => {
+          return doc.title[0].value
+        },
+        maxLength: 96,
+
+      },
+      validation: Rule => Rule.required()
+    }),
+    defineField({
       name: 'title',
       type: 'internationalizedArrayString',
       ...requiredAllLanguagesValidation,
